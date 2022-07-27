2d8b7068b9c3039e28db3daa8f718bd37fad2fc2
Claudio Carnino <claudio@tugulab.org>
2022-07-27 10:38:05 +0100

    Initial Commit

---
+++ IOC/IOC.h
@@ -0,0 +1,18 @@
+//
+//  IOC.h
+//  IOC
+//
+//  Created by Claudio Carnino on 27/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOC.
+FOUNDATION_EXPORT double IOCVersionNumber;
+
+//! Project version string for IOC.
+FOUNDATION_EXPORT const unsigned char IOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOC/PublicHeader.h>
+
+
